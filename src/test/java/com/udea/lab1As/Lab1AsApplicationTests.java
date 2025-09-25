4dc380ffcfe1cec5209033315b73f3eef00fc0ac
RUTENCO <daniel.ramirez7@udea.edu.co>
2025-09-25 18:19:33 -0500

    base project

---
+++ src/test/java/com/udea/lab1As/Lab1AsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.lab1As;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1AsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
